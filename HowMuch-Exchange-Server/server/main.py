65417db6603d4c37f73eebb3675a7bde07a9bd26
JoMingyu <city7310@naver.com>
2017-03-22 19:32:55 +0900

    main 모듈 index path 통신 테스트 완료

--- HowMuch-Exchange-Server/server/main.py
+++ HowMuch-Exchange-Server/server/main.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/', methods=['POST', 'GET'])
+def test():
+    return 'success to connect'
+
 if __name__ == "__main__":
     print('서버 시작')
-    app.run('0.0.0.0')
+    app.run('10.156.145.120')
