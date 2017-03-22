57178501112bcf09038731d7910e706af5e37575
JoMingyu <city7310@naver.com>
2017-03-22 20:29:30 +0900

    오늘 일 완료

--- HowMuch-Exchange-Server/server/main.py
+++ HowMuch-Exchange-Server/server/main.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import request, make_response
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def test():
-    return 'success to connect'
+# @app.route('/', methods=['POST', 'GET'])
+# def test():
+#     return 'success to connect'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print('서버 시작')
     app.run('10.156.145.120')
