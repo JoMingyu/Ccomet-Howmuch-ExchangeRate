c287a4a9f4180be3124eeb75de901e4958dfcbb8
JoMingyu <city7310@naver.com>
2017-04-03 13:09:28 +0900

    check_time 메소드 삭제, term 기반으로 스레드를 돌림

--- HowMuch-Exchange-Server/support/push_thread.py
+++ HowMuch-Exchange-Server/support/push_thread.py
@@ -8,35 +8,18 @@ class PushThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
 
-        self.last_send = self.current_time()
-        self.term = 1
+        self.term = 5
         self.p = Parser()
 
     def run(self):
         print('Push thread start')
-        if self.check_time():
-            self.last_send = self.current_time()
 
-            for country_code in self.p.code_list:
-                json_data = self.p.get_exchange_rate(country_code)
-                rate_list = self.p.process_data(json_data)
+        for country_code in self.p.code_list:
+            json_data = self.p.get_exchange_rate(country_code)
+            rate_list = self.p.process_data(json_data)
 
-                for exchange_rate in rate_list:
-                    print(exchange_rate)
-                    self.p.commit_data(exchange_rate)
+            for exchange_rate in rate_list:
+                print(exchange_rate)
+                self.p.commit_data(exchange_rate)
 
-        else:
-            time.sleep(self.term)
-
-    @staticmethod
-    def current_time():
-        return datetime.datetime.now()
-
-    def check_time(self):
-        current_time = self.current_time()
-        time = self.last_send - current_time
-
-        if time.seconds >= 300:
-            return True
-        else:
-            return False
+        time.sleep(self.term * 60)
