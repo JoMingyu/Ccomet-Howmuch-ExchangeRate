03bbb9e3b0702bc4f1a0b58a007f51a075874e71
zhsir123 <zhsir123@gmail.com>
2017-05-15 13:50:46 +0900

    그래프 그릴 떄 쓸 데이터

--- HowMuch-Exchange-Server/database/database.py
+++ HowMuch-Exchange-Server/database/database.py
@@ -11,7 +11,7 @@ from pymysql import IntegrityError
 class Database:
     host = 'localhost'
     user = 'root'
-    password = 'dhk0654'
+    password = ''
     db = 'howmuch_exchange'
     charset = 'utf8'
 
